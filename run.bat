3c348a920163eb191d0a802afdf0955f03f40a48
linhsama <fdcvlinh6@gmail.com>
2026-08-15 09:43:38 +0700

    chore: update run.bat title

--- run.bat
+++ run.bat
@@ -3,8 +3,7 @@ title Tra Cuu Diem Thi THPT & Tinh Diem Hoc Ba 2026 - Local Server
 cd /d "%~dp0"
 echo ========================================================
 echo   KHOI DONG MAY CHU CUC BO TRA CUU DIEM THI 2026
-echo   Dia chi: http://127.0.0.1:8080/
-echo   Ho tro tra cuu sieu toc - Khong bi chan CORS!
+echo   Dia chi: http://[IP_ADDRESS]/
 echo ========================================================
 python server.py
 pause
